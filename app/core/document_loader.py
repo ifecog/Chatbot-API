893cb0ea32e12b8c2658ad16ad0845c635dc76a0
Ifeoluwa Ilori <ifeoluwasamson90@gmail.com>
2025-06-12 22:27:12 +0100

    Fix code errors

--- app/core/document_loader.py
+++ app/core/document_loader.py
@@ -2,3 +2,24 @@ import os
 from langchain.document_loaders import PyPDFLoader, TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
+
+
+# def load_and_split_docs(file_path: str):
+#     ext = os.path.splitext(file_path)[1].lower()
+    
+#     if ext == '.pdf':
+#         loader = PyPDFLoader(file_path)
+#     elif ext == '.txt' or ext == '.md':
+#         loader = TextLoader(file_path)
+#     else:
+#         raise ValueError('Unsupported file format. Only PDF, TXT, and MD filel formats are supported')
+    
+#     documents = loader.load()
+    
+#     splitter = RecursiveCharacterTextSplitter(
+#         chunk_size=500,
+#         chunk_overlap=100
+#     )
+    
+#     split_docs = splitter.split_documents(documents)
+#     return split_docs
