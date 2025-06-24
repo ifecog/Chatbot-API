de05fe75ad33dcde8069c51e1b0f3d55a8fdc51a
Ifeoluwa Ilori <ifeoluwasamson90@gmail.com>
2025-06-24 15:26:48 +0100

    Add dependency function to get current user

---
+++ app/utils/auth.py
@@ -0,0 +1,20 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from app.utils.security import decode_access_token
+from app.crud.user import get_user_by_email
+from app.dependencies import get_db
+from app.models.user import User
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/signin")
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    payload = decode_access_token(token)
+    if not payload:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid token')
+    
+    user = db.query(User).get(payload.get('sub'))
+    if not use:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found.')
+
+    return user
